5b959f21af1c28918062f02b20ea112beb555e06
sayborduu <badimon.alex@gmail.com>
2026-07-25 15:29:14 +0200

    overwrite backupview

--- SayStore/Views/Settings/Backup/BackupView.swift
+++ SayStore/Views/Settings/Backup/BackupView.swift
@@ -1,41 +1,26 @@
-// MARK: - View
-import NimbleViews
 //
-//  ArchiveView.swift
-//  Feather
+//  BackupView.swift
+//  SayStore
 //
-//  Created by samara on 6.05.2025.
+//  Created by Alex Badi on 25/07/2026.
 //
 
 import SwiftUI
-
 import Zip
+import NimbleViews
 
+// MARK: - View
 struct ArchiveView: View {
-  //@AppStorage("SayStore.useShareSheetForArchiving") private var _useShareSheet: Bool = false
-
-  // MARK: Body
-  var body: some View {
-    NBList(.localized("Backup")) {
-      Section {
-        Picker(
-          .localized("Compression Level"), systemImage: "archivebox", selection: $_compressionLevel
-        ) {
-          ForEach(ZipCompression.allCases, id: \.rawValue) { level in
-            Text(level.label).tag(level)
-          }
-        }
-      }
-
-      Section {
-        Toggle(
-          .localized("Show Sheet when Exporting"), systemImage: "square.and.arrow.up",
-          isOn: $_useShareSheet)
-      } footer: {
-        Text(
-          .localized(
-            "Toggling show sheet will present a share sheet after exporting to your files."))
-      }
+	//@AppStorage("SayStore.useShareSheetForArchiving") private var _useShareSheet: Bool = false
+	
+	// MARK: Body
+    var body: some View {
+		NBList(.localized("Backup")) {
+			Section {
+				Button(.localized("Open Documents"), systemImage: "folder") {
+					UIApplication.open(URL.documentsDirectory.toSharedDocumentsURL()!)
+				}
+			}
+		}
     }
-  }
 }
